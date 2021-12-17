fee6844acad0680cab30a25b229cfe8b6dbe054c
Raoni Madeira <raonimadeira@gboex.com.br>
2021-12-17 01:25:03 -0300

    initial-version

---
+++ Cliente.js
@@ -0,0 +1,13 @@
+export class Cliente{
+    _nome;
+    _cpf;
+
+    constructor(nome, cpf){
+        this._nome = nome;
+        this._cpf = cpf;
+    }
+
+    get cpf(){
+        return this._cpf;
+    }
+}
